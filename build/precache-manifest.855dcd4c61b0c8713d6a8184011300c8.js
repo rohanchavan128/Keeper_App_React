dc756fd4ab1cfc74943b0af835cba59dbc9ff21d
rohanchavan128 <rohanchavan128@gmail.com>
2020-03-16 23:22:35 +0530

    2nd commit

---
+++ build/precache-manifest.855dcd4c61b0c8713d6a8184011300c8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717171709b412ab211ad9ec418e04c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "335cdb81bd7a1a9768e0",
+    "url": "/static/js/2.b9176560.chunk.js"
+  },
+  {
+    "revision": "5845ef81b7fb76fbc7df",
+    "url": "/static/js/main.f487ca59.chunk.js"
+  },
+  {
+    "revision": "9c4d8a51a87689431b1d",
+    "url": "/static/js/runtime-main.7e082221.js"
+  }
+]);
